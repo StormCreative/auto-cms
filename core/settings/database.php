236ed2cf53ed2336d9e7756d22817a3545defbbe
David Jones <davidjones@stormcreative.co.uk>
2013-10-07 17:58:28 +0100

    Update database.php

--- core/settings/database.php
+++ core/settings/database.php
@@ -6,12 +6,6 @@ if (!LIVE) {
     $settings[ 'DB_USER' ] = 'root';
     $settings[ 'DB_PASS' ] = 'root';
 } else {
-	/**
-    $settings[ 'DB_HOST' ] = '78.109.163.36';
-    $settings[ 'DB_NAME' ] = 'stormtest_';
-    $settings[ 'DB_USER' ] = 'dbu_stormtest';
-    $settings[ 'DB_PASS' ] = 'dre+rATr';
-    **/
 
     $settings[ 'DB_HOST' ] = 'localhost';
     $settings[ 'DB_NAME' ] = 'pegisis';
